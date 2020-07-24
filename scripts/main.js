b98d8b0b27f63f0422d1fa8e681aa0b3658e1812
Brenda Boudaie <bboudaie@gmail.com>
2020-07-23 18:33:20 -0700

    new mobile pictures

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+// var tabletImages = document.getElementsByClassName("tablet-view-img");
+
+// window.onresize = function() {
+//     if (window.innerWidth <= 767) {  
+//         console.log("Mobile");
+//         for (i = 0; i < tabletImages.length; i++){
+//             tabletImages[i].remove();
+//             console.log("removing " + tabletImages[i]);
+
+//         }
+//     }
+// }
+
+
+// console.log("Test");

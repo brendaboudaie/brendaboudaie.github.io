d284dbfdd83ccbc7711fe85931d8d4b152f467c5
Brenda Boudaie <bboudaie@gmail.com>
2020-08-06 18:42:42 -0700

    about section + nav background

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,11 @@
-// var tabletImages = document.getElementsByClassName("tablet-view-img");
-
-// window.onresize = function() {
-//     if (window.innerWidth <= 767) {  
-//         console.log("Mobile");
-//         for (i = 0; i < tabletImages.length; i++){
-//             tabletImages[i].remove();
-//             console.log("removing " + tabletImages[i]);
-
-//         }
-//     }
-// }
-
-
-// console.log("Test");
+$(document).ready(function(){
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        if (scroll > 15) {
+          $(".top-nav").css("background" , "white");
+        }
+        else{
+            $(".top-nav").css("background" , "transparent");  	
+        }
+    })
+  })

4bffcad923f09f34153795dd41207dfd8cd8b719
Brenda Boudaie <bboudaie@gmail.com>
2020-09-08 18:20:22 -0700

    logo change

--- scripts/main.js
+++ scripts/main.js
@@ -4,9 +4,13 @@ $(document).ready(function(){
         var scroll = $(window).scrollTop();
         if (scroll > 15) {
           $(".top-nav").css("background" , "white");
+          $(".logoname").css("display", "none");
+          $(".logo-img").css("display", "block");
         }
         else{
             $(".top-nav").css("background" , "transparent");  	
+            $(".logoname").css("display", "block");
+            $(".logo-img").css("display", "none");
         }
     })
   })

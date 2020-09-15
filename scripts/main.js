9511be4ea6a9a703ee31438dba8d1aefb5338f24
Brenda Boudaie <bboudaie@gmail.com>
2020-09-14 21:25:03 -0700

    resizing about

--- scripts/main.js
+++ scripts/main.js
@@ -4,9 +4,11 @@ $(document).ready(function(){
         var scroll = $(window).scrollTop();
         if (scroll > 15) {
           $(".top-nav").css("background" , "white");
+          $(".top-nav").css("box-shadow" , "0px 4px 10px 0px rgba(0,0,0,0.30)");
         }
         else{
-            $(".top-nav").css("background" , "transparent");  	
+            $(".top-nav").css("background" , "transparent");  
+            $(".top-nav").css("box-shadow" , "none");
         }
     })
   })

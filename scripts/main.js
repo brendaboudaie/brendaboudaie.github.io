ecf2e06b26c3751acda5c17791c33c09d1818bc4
Brenda Boudaie <bboudaie@gmail.com>
2020-11-09 13:16:59 -0800

    new work page

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,15 @@
 // NAV BAR SCROLL
-$(document).ready(function(){
-    $(window).scroll(function(){
-        var scroll = $(window).scrollTop();
-        if (scroll > 15) {
-          $(".top-nav").css("background" , "white");
-          $(".top-nav").css("box-shadow" , "0px 4px 10px 0px rgba(0,0,0,0.30)");
-        }
-        else{
-            $(".top-nav").css("background" , "transparent");  
-            $(".top-nav").css("box-shadow" , "none");
-        }
-    })
-  })
+// $(document).ready(function(){
+//     $(window).scroll(function(){
+//         var scroll = $(window).scrollTop();
+//         if (scroll > 15) {
+//           $(".top-nav").css("background" , "white");
+//           $(".top-nav").css("box-shadow" , "0px 4px 10px 0px rgba(0,0,0,0.30)");
+//         }
+//         else{
+//             $(".top-nav").css("background" , "transparent");  
+//             $(".top-nav").css("box-shadow" , "none");
+//         }
+//     })
+//   })
+
